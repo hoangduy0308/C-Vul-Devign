bc60b350b1fca64fdd133e8c1580aded012cb56e
Nguyen Le Hoang Duy <39380551+hoangduy0308@users.noreply.github.com>
2025-12-27 18:04:43 +0700

    Delete codebert directory

--- codebert/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-CodeBERT-based Vulnerability Detection for C/C++ Code.
-
-This package provides tools for training and inference using CodeBERT
-to detect vulnerabilities in source code.
-"""
-
-__version__ = "0.1.0"
